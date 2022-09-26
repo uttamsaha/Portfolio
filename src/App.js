6258bbc8514ded3ab36c0e29075f5783cb615f35
uttamsaha <uttamsaha982@live.com>
2022-09-26 19:35:13 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './Components/Portfolio/Nav/Nav';
+import Home from './Components/Portfolio/Home/Home';
 import About from './Components/Portfolio/About/About';
 import Services from './Components/Portfolio/Services/Services';
 import Projects from './Components/Portfolio/Projects/Projects';
 import Contact from './Components/Portfolio/Contact/Contact';
 import Footer from './Components/Portfolio/Footer/Footer';
-// import Home from './Components/Portfolio/Home/Home';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
-      {/* <Home></Home> */}
+      <Home></Home>
       <About></About>
       <Services></Services>
       <Projects></Projects>
