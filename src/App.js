ceb43328807155ee324850dff219e731e6b73399
uttamsaha <uttamsaha982@live.com>
2022-09-26 19:20:02 +0600

    nav added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './Components/Portfolio/Nav/Nav';
+// import Home from './Components/Portfolio/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav></Nav>
+      {/* <Home></Home> */}
     </div>
   );
 }
