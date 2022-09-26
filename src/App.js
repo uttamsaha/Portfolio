fda1ccd30542e6eca0ca30e5e17712b8cebe7294
uttamsaha <uttamsaha982@live.com>
2022-09-26 19:31:12 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './Components/Portfolio/Nav/Nav';
 import About from './Components/Portfolio/About/About';
+import Services from './Components/Portfolio/Services/Services';
+import Projects from './Components/Portfolio/Projects/Projects';
+import Contact from './Components/Portfolio/Contact/Contact';
+import Footer from './Components/Portfolio/Footer/Footer';
 // import Home from './Components/Portfolio/Home/Home';
 
 function App() {
@@ -10,6 +14,10 @@ function App() {
       <Nav></Nav>
       {/* <Home></Home> */}
       <About></About>
+      <Services></Services>
+      <Projects></Projects>
+      <Contact></Contact>
+      <Footer></Footer>
 
     </div>
   );
